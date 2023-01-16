1866ebd25724befc2fe19f06601b700e3f114d2a
Pedro <palfo@ecomp.poli.br>
2023-01-16 01:05:18 -0300

    Formatando 'Post' de Produto

--- src/hooks/useProduto.ts
+++ src/hooks/useProduto.ts
@@ -1,97 +1,109 @@
-import Produto from "../core/Produto"
-import { useState,useEffect,useMemo  } from 'react'
-import ColecaoProduto from '../backend/bd/ColecaoProduto'
-import useTabelaOuForm from './useTabelaOuForm'
-import {PostProduto,GetProduto, ExcluirProduto, UpdateProduto} from '../backend/bd/ResquestsProdutos'
+import Produto from "../core/Produto";
+import { useState, useEffect, useMemo } from "react";
+import ColecaoProduto from "../backend/bd/ColecaoProduto";
+import useTabelaOuForm from "./useTabelaOuForm";
+import {
+  PostProduto,
+  GetProduto,
+  ExcluirProduto,
+  UpdateProduto,
+} from "../backend/bd/ResquestsProdutos";
 
-export default function useProduto(){
+export default function useProduto() {
+  const {
+    exibirTabela,
+    exibirFormulario,
+    formularioVisivel,
+    tabelaVisivel,
+    ordenacao,
+    setOrdenacao,
+    alterarOrdenacao,
+    getClassNamesFor,
+  } = useTabelaOuForm();
 
-    const { 
-        exibirTabela,
-        exibirFormulario,
-        formularioVisivel,
-        tabelaVisivel,
-        ordenacao,
-        setOrdenacao,
-        alterarOrdenacao,
-        getClassNamesFor
-      } = useTabelaOuForm()
+  const [Produto, setProduto] = useState([]);
+  const [ProdutoDup, setProdutoDup] = useState([]);
+  const [Produtos, setProdutos] = useState([]);
 
-    const [Produto,setProduto]        = useState([])
-    const [ProdutoDup,setProdutoDup]  = useState([])
-    const [Produtos,setProdutos]      = useState([])
+  useEffect(obterTodos, []);
 
-    useEffect(obterTodos, [])
-  
-    function obterTodos(){
-        GetProduto().then(prods => {
-          setProdutos(prods)
-          exibirTabela()
-        })
-    }
-  
-    function editarProduto(Produto){
-      setProduto(Produto)
-      setProdutoDup([])
-      exibirFormulario()
-    }
-  
-    async function excluirProduto(Produto){
-      await ExcluirProduto(Produto.id)
-      obterTodos();
-    }
+  function obterTodos() {
+    GetProduto().then((prods) => {
+      setProdutos(prods);
+      exibirTabela();
+    });
+  }
 
-     
-    function duplicarProduto(Produto){
-      setProdutoDup(Produto)
-      setProduto([])
-      exibirFormulario()
-    }
-    
-    async function salvarProduto(id,codigo,nome,preco,categoria,unidade){
-      console.log('id edit',id)
-        id? 
-          (UpdateProduto(id,codigo,nome,preco,categoria,unidade) .then(resp => {
+  function editarProduto(Produto) {
+    setProduto(Produto);
+    setProdutoDup([]);
+    exibirFormulario();
+  }
+
+  async function excluirProduto(Produto) {
+    await ExcluirProduto(Produto.id);
+    obterTodos();
+  }
+
+  function duplicarProduto(Produto) {
+    setProdutoDup(Produto);
+    setProduto([]);
+    exibirFormulario();
+  }
+
+  async function salvarProduto({
+    id,
+    codigo,
+    nome,
+    preco,
+    categoria,
+    unidade,
+  }) {
+    console.log("id edit", id);
+    id
+      ? UpdateProduto({ id, codigo, nome, preco, categoria, unidade }).then(
+          (resp) => {
             obterTodos();
-          })):(
-          PostProduto(codigo,nome,preco,categoria,unidade)
-        .then(resp => {
-          obterTodos();
-        }))
-    }
-  
-    function novoProduto(){
-      setProduto([])
-      setProdutoDup([])
-      exibirFormulario()
-    }
+          }
+        )
+      : PostProduto({ codigo, nome, preco, categoria, unidade }).then(
+          (resp) => {
+            obterTodos();
+          }
+        );
+  }
 
-    function TratarVariavel(variavel){
-      console.log(variavel+" - tipo : "+typeof variavel)
-      if(typeof variavel == "string") {
-        return variavel.toLowerCase();
-      } 
-      else {
-        return variavel;
-      }
-    }
+  function novoProduto() {
+    setProduto([]);
+    setProdutoDup([]);
+    exibirFormulario();
+  }
 
-    return{
-        Produto,
-        Produtos,
-        novoProduto,
-        salvarProduto,
-        excluirProduto,
-        editarProduto,
-        duplicarProduto,
-        obterTodos,
-        formularioVisivel,
-        tabelaVisivel,
-        exibirTabela,
-        ordenacao,
-        setOrdenacao,
-        alterarOrdenacao,
-        getClassNamesFor,
-        ProdutoDup
+  function TratarVariavel(variavel) {
+    console.log(variavel + " - tipo : " + typeof variavel);
+    if (typeof variavel == "string") {
+      return variavel.toLowerCase();
+    } else {
+      return variavel;
     }
+  }
+
+  return {
+    Produto,
+    Produtos,
+    novoProduto,
+    salvarProduto,
+    excluirProduto,
+    editarProduto,
+    duplicarProduto,
+    obterTodos,
+    formularioVisivel,
+    tabelaVisivel,
+    exibirTabela,
+    ordenacao,
+    setOrdenacao,
+    alterarOrdenacao,
+    getClassNamesFor,
+    ProdutoDup,
+  };
 }
