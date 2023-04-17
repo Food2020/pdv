20a143d058b581c1f50d8b331aa7fde5a9810da6
Elvinho <elvison.victor@liferay.com>
2023-04-16 23:15:01 -0300

    login atraves do primsa

---
+++ src/pages/api/usuario/login.ts
@@ -0,0 +1,56 @@
+import UserController from "../../../backend/bd/UserController"
+import SuperJSON from "superjson";
+const bcrypt = require("bcryptjs");
+
+
+export default async function handler(req,res){
+    const userController = new UserController();
+
+    if(req.method === "POST"){
+
+       const data = req.body;
+       const usuario =  await loginUser(userController,data);
+       const usuarioLogado = JSON.parse(SuperJSON.stringify(usuario))
+
+       if(usuarioLogado === null){
+         return res.status(400).json({
+            erro:true,
+            message:"Erro: Usuario ou Senha Incorreta !",
+            status:res.status,
+         });
+       }
+
+       if(data.email !== usuarioLogado.json.email){
+        return res.status(400).json({
+            error: true,
+            mensagem: "Erro: Usuário ou senha incorreto! ",
+          });
+
+       }
+
+       console.log(data.senha.toString()," ",usuarioLogado.json.senha.toString())
+
+       if(!(await bcrypt.compare(data?.senha.toString(),usuarioLogado?.json?.senha.toString()))){
+
+        return res.status(400).json({
+            erro:true,
+            message:"Erro: Usuario ou Senha Incorreta! ",
+            status:res.status,
+        })
+        
+       }
+
+      const {nome, id} = usuarioLogado.json;
+      return res.json({
+        erro:false,
+        message:"Login Realizado com Sucesso!",
+        usuarioLogado:{nome,id},
+      });
+
+    }
+
+}
+
+async function loginUser(userController,data){
+    return await userController.login(data);
+  }
