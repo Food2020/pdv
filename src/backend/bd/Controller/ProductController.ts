c4835468e67d1728cdb6e87c473a8fc9af7a29fa
Elvinho <elvison.victor@liferay.com>
2023-04-19 23:19:55 -0300

    arquivo controlleruser

--- src/backend/bd/Controller/ProductController.ts
+++ src/backend/bd/Controller/ProductController.ts
@@ -1,4 +1,6 @@
 import { PrismaClient, products } from "@prisma/client";
+import PreviousMap from "postcss/lib/previous-map";
+import SuperJSON from "superjson";
 
 export default class UserController {
 
@@ -8,49 +10,82 @@ export default class UserController {
     }
     async get(){
         try{
-            return this.prisma.products.findMany();
+            const produtos = this.prisma.products.findMany();
+            return produtos;
         }catch(e){
             return e
         }
     }
     async create(data:products){
         try{
-           return this.prisma.products.create({
+           const produto = this.prisma.products.create({
                 data:{
-                    ...data,
+                   nome:data.nome.toString(),
+                   preco:Number(data.preco),
+                   codigo:data.codigo.toString(),
+                   categoria:data.categoria.toString(),
+                    unidade:data.unidade.toString(),
+                    ativo:Number(data.ativo),
                     createdAt:new Date(),
                     updatedAt:new Date()
-                }as any
+                }
             })
+            return SuperJSON.stringify({
+                id:(await produto).id,
+                nome:(await produto).nome,
+                preco:(await produto).preco,
+                codigo:(await produto).codigo,
+                categoria:(await produto).categoria,
+                unidade:(await produto).unidade,
+                ativo:(await produto).ativo,
+                createdAt:(await produto).createdAt
+            });
         }catch(e){
             return e;
         }
     }
 
     async update(data:products){
-        try{
-            return this.prisma.products.update({
+
+          try{ 
+             const produto =  this.prisma.products.update({
                 where:{
                     id:Number(data.id),
                 },
                 data:{
-                   ...data,
+                    nome:data.nome,
+                    preco:Number(data.preco),
+                    codigo:data.codigo.toString(),
+                    categoria:data.categoria.toString(),
+                    unidade:data.unidade.toString(),
+                    ativo:Number(1),
                     updatedAt:new Date()
                 }
            })
-           
+           return SuperJSON.stringify({
+            id:(await produto).id,
+            nome:(await produto).nome,
+            preco:(await produto).preco,
+            codigo:(await produto).codigo,
+            categoria:(await produto).categoria,
+            unidade:(await produto).unidade,
+            ativo:(await produto).ativo,
+            createdAt:(await produto).createdAt,
+            updateAt:(await produto).updatedAt
+           })
         }catch(e){
-            return e
+            return e;
         }
-
-    }
+        }
 
     async delete(data:products){
         try{
-           return this.prisma.products.delete({
-                where:{
-                id:Number(data.id)
-            }})
+           const produto =  this.prisma.products.delete({
+                            where:{
+                            id:Number(data.id)
+                            }  
+                        });
+            return (produto);
         }catch(e){
             return e;
         }
