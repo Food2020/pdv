e657a0b676ec2c9788bc68b4815b66c694ddc157
Elvinho <elvison.victor@liferay.com>
2023-05-05 22:56:42 -0300

    alterar icone Estoque

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -277,21 +277,14 @@ export const IconeDuplicar = (
 	</svg>
 );
 export const IconeEstoque = (
-	<svg
-		fill="none"
-		viewBox="0 0 24 24"
-		height="1em"
-		width="1em"
-		className="h-6 w-6"
-	>
-		<path fill="currentColor" d="M10 12a1 1 0 100 2h4a1 1 0 000-2h-4z" />
-		<path
-			fill="currentColor"
-			fillRule="evenodd"
-			d="M4 2a3 3 0 00-3 3v14a3 3 0 003 3h16a3 3 0 003-3V5a3 3 0 00-3-3H4zm16 2H4a1 1 0 00-1 1v3h18V5a1 1 0 00-1-1zM3 19v-9h18v9a1 1 0 01-1 1H4a1 1 0 01-1-1z"
-			clipRule="evenodd"
-		/>
-	</svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+  <path 
+  	strokeLinecap="round" 
+	strokeLinejoin="round" 
+	d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9 "/>
+	
+</svg>
+
 );
 
 export const IconeUsuario = (
